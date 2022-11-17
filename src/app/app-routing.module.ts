1c0e0e6466607550af01a4d8cd50aea27673e7ac
Meece <meece.work@gmail.com>
2022-11-17 13:36:34 +0200

    Added a divider in the showcase component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PokemonCardComponent } from './components/pokemon-showcase/pokemon-card/pokemon-card.component';
+import { PokemonShowcaseComponent } from './components/pokemon-showcase/pokemon-showcase.component';
 
 const appRoutes: Routes = [
-  { path: 'pokemon-list', component: PokemonCardComponent },
-  { path: 'favorites', component: PokemonCardComponent },
+  { path: 'pokemon-list', component: PokemonShowcaseComponent },
+  { path: 'favorites', component: PokemonShowcaseComponent },
   { path: '**', redirectTo: 'pokemon-list' }
 ];
 
